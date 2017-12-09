c7d20effea0dea5686ee9babff51eb84b5f70c2a
Marco Piedra Venegas <mpiedrav@ucenfotec.ac.cr>
2017-12-09 07:33:36 -0600

    Initial commit

---
+++ src/com/cenfotec/pizzaservice/example/PizzaExample.java
@@ -0,0 +1,19 @@
+package com.cenfotec.pizzaservice.example;
+
+public class PizzaExample {
+	public static void main(String[] args) {
+		runBuilderExample();
+		runDecoratorExample();
+	}
+	
+	public static void runBuilderExample() {
+		Pizza pizza1;
+		PizzaBuilder pizzabuilder1;
+		
+		
+	}
+	
+	public static void runDecoratorExample() {
+		
+	}
+}
